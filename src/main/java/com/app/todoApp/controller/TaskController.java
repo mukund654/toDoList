8864d39b7399224fee6567c53f69319e227fd0b2
mukund654 <mukundmaheshwari1568@gmail.com>
2025-08-16 16:09:45 +0530

    project is complete

--- src/main/java/com/app/todoApp/controller/TaskController.java
+++ src/main/java/com/app/todoApp/controller/TaskController.java
@@ -1,15 +1,60 @@
 package com.app.todoApp.controller;
 
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.app.todoApp.TodoAppApplication;
+import com.app.todoApp.models.Task;
 import com.app.todoApp.services.TaskService;
 
 @Controller
+//@RequestMapping("/tasks")
 public class TaskController {
 
+    private final TodoAppApplication todoAppApplication;
+
     private final TaskService ts;
-    public TaskController(TaskService ts) {
+    public TaskController(TaskService ts, TodoAppApplication todoAppApplication) {
         this.ts = ts;
+        this.todoAppApplication = todoAppApplication;
     }
+
+    // the tasks we returning as a string is a thymeleaf template and we are passing tasks to it
+    @GetMapping
+    public String getTasks(Model model) {
+        List<Task> tasks = ts.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+    
+    @PostMapping
+    public String createTasks(@RequestParam  String title) {
+        ts.createTasks(title);
+        
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/{id}/delete") 
+    public String deleteTasks(@PathVariable Long id) {
+        
+        ts.deleteTasks(id);
+        return "redirect:/";
+    }
+
+     @GetMapping("/{id}/toggle") 
+    public String toggleTasks(@PathVariable Long id) {
+        
+        ts.toggleTasks(id);
+        return "redirect:/";
+    }
+
+
 }
--- src/main/java/com/app/todoApp/services/TaskService.java
+++ src/main/java/com/app/todoApp/services/TaskService.java
@@ -1,8 +1,61 @@
 package com.app.todoApp.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.app.todoApp.Repository.TaskRepository;
+import com.app.todoApp.models.Task;
+
 
 @Service
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'getAllTasks'");
+        // we will gets the tasks from data base using repository
+
+
+        return taskRepository.findAll();
+        // this returns list of all tasks
+        
+
+
+    }
+
+    public void createTasks(String title) {
+        // // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'createTasks'");
+
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
+
+    public void deleteTasks(Long id) {
+        // // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'deleteTasks'");
+
+        taskRepository.deleteById(id);
+         
+    }
+
+    public void toggleTasks(Long id) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'toggleTasks'");
+
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid task ID"));
+        task.setCompleted(!task.isCompleted());
+        taskRepository.save(task);
+    }
 
 }
